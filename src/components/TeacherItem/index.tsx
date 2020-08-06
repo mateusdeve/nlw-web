780b46b963a1f6f899473d0ca601ee49a1fd9be8
Mateus Pires <mateus.dev.ti@gmail.com>
2020-08-06 17:43:05 -0300

    Projeto finalizado *-*

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -2,33 +2,58 @@ import React from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 import './styles.css'
-function TeacherItem(){
+import api from '../../services/api';
+
+export interface Teacher {
+    id: number,
+    avatar: string,
+    bio: string,
+    cost: number,
+    name: string,
+    subject: string,
+    whatsapp: string
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+
+const TeacherItem: React.FC<TeacherItemProps> = ({teacher}) => {
+    
+    function createNewConnection(){
+        api.post('/connections', {
+            user_id: teacher.id,
+        })
+    }
+
     return(
         <article className="teacher-item">
                 <header>
-                    <img src="https://avatars3.githubusercontent.com/u/55514882?s=400&u=53431660bdc6db0ea96e8ba4e47a868df8052e05&v=4" alt=""/>
+                    <img src={teacher.avatar} alt={teacher.name}/>
                     <div>
                         <strong>
-                            Mateus Silveira Pires
+                            {teacher.name}
                         </strong>
-                        <span>Programação</span>
+                        <span>{teacher.subject}</span>
                     </div>
                 </header>
                 <p>
-                    Entusiasta das melhores tecnologias 
-                    <br />
-                    <br />
-                    Apaixonador por codar
+                   {teacher.bio}
                 </p>
                 <footer>
                     <p>
                         preço/hora
-                        <strong>R$ 30,00</strong>
+                        <strong>{teacher.cost}</strong>
                     </p>
-                    <button type="button">
+                    <a
+                    onClick={createNewConnection}
+                    target="_blank"
+                    href={`https://wa.me/${teacher.whatsapp}`}
+                    >
                         <img src={whatsappIcon} alt="WhatsApp"/>
                         Entrar em contato
-                    </button>
+                    </a>
                 </footer>
             </article>
     )
--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './styles.css'
 
 import logoImg from '../../assets/images/logo.svg';
@@ -7,8 +7,21 @@ import studyIcon from '../../assets/images/icons/study.svg';
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg'; 
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg'; 
 import { Link } from 'react-router-dom';
+import api from '../../services/api';
 
 function Landing () {
+
+    const [totalConnections, setTotalConnections] = useState(0);
+
+    useEffect(() => {
+        api.get('/connections').then(response => {
+            const { total } = response.data;
+
+            setTotalConnections(total);
+        })
+    }, [] )
+
+
     return (
         <div id="page-landing">
             <div id="page-landing-content" className="container">
@@ -28,7 +41,7 @@ function Landing () {
                     </Link>
                 </div>
                 <span className="total-connections">
-                    Total de 200 conexões já realixadas
+                    Total de {totalConnections} conexões já realixadas
                     <img src={purpleHeartIcon} alt="Coração roxo"/>
                 </span>
             </div>
